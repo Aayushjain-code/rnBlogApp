f1cc9d97d39f9f950196e0b029ce86f498e56fac
Aayus Jain <aayushjain6020@gmail.com>
2022-01-14 17:50:14 +0530

    initialized

---
+++ App.js
@@ -0,0 +1,22 @@
+import * as React from 'react';
+import { Button, View, Text } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import IndexScreen from './src/screens/IndexScreen';
+
+
+
+
+const Stack = createNativeStackNavigator();
+
+function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Index">
+        <Stack.Screen name="Index" component={IndexScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+export default App;
