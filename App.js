ce1a249c6957f3f95d48e00a22f04e206c7e49fe
Aayus Jain <aayushjain6020@gmail.com>
2022-01-15 17:42:09 +0530

    Create Part Done

--- App.js
+++ App.js
@@ -3,6 +3,7 @@ import { Button, View, Text } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import IndexScreen from './src/screens/IndexScreen';
+import { Provider } from './src/context/BlogContext';
 
 
 
@@ -11,12 +12,40 @@ const Stack = createNativeStackNavigator();
 
 function App() {
   return (
-    <NavigationContainer>
-      <Stack.Navigator initialRouteName="Index">
-        <Stack.Screen name="Index" component={IndexScreen} />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <Provider>
+      <NavigationContainer>
+        <Stack.Navigator initialRouteName="Blogs">
+          <Stack.Screen name="Blogs" component={IndexScreen} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </Provider>
+
   );
 }
 
 export default App;
+
+
+// import React from 'react';
+// import { createAppContainer } from 'react-navigation';
+// import { createStackNavigator } from 'react-navigation-stack';
+
+// import IndexScreen from './src/screens/IndexScreen';
+
+// const navigator = createStackNavigator(
+//   {
+//     Index: IndexScreen,
+//   },
+//   {
+//     initialRouteName: 'Index',
+//     defaultNavigationOptions: {
+//       title: 'Blogs',
+//     },
+//   }
+// );
+
+// const App = createAppContainer(navigator);
+
+// export default () => {
+//   return <App />;
+// };
--- src/screens/IndexScreen.js
+++ src/screens/IndexScreen.js
@@ -1,14 +1,47 @@
-import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import React, { useContext } from 'react';
+import { View, Text, StyleSheet, FlatList, Button, TouchableOpacity } from 'react-native';
+import { Context } from '../context/BlogContext';
+import Icon from 'react-native-vector-icons/FontAwesome';
 
 const IndexScreen = () => {
+  const { state, addBlogPost } = useContext(Context);
+
   return (
     <View>
-      <Text>Index Screen</Text>
+      <Button title="Add Post" onPress={addBlogPost} />
+      <FlatList
+        data={state}
+        keyExtractor={blogPost => blogPost.title}
+        renderItem={({ item }) => {
+          return (
+            <View style={styles.row}>
+              <Text style={styles.title}>{item.title}-{item.id}</Text>
+              <TouchableOpacity onPress={()=>console.log(item.id)}>
+              <Icon name="trash" size={30} color="#000" />
+              </TouchableOpacity>
+            </View>
+          );
+        }}
+      />
     </View>
   );
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  row: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    paddingVertical: 20,
+    paddingHorizontal: 10,
+    borderTopWidth: 1,
+    borderColor: 'gray'
+  },
+  title: {
+    fontSize: 18
+  },
+  icon: {
+    fontSize: 24
+  }
+});
 
 export default IndexScreen;
